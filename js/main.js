a80b09d7aebe0a3c321badcc58b6259bba90bce7
oops-shaun <shaun_lv@163.com>
2025-04-17 22:50:34 +0800

    Site updated: 2025-04-17 22:50:34

--- js/main.js
+++ js/main.js
@@ -2,22 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   let headerContentWidth, $nav
   let mobileSidebarOpen = false
 
-  // APlayer 默认关闭歌词
-  // 创建一个 MutationObserver 实例，用于监听 DOM 的变化
-  var observer = new MutationObserver(function (mutations) {
-    // 查找页面中 class 为 "aplayer-icon-lrc" 的元素
-    var lrcButton = document.querySelector(".aplayer-icon-lrc");
-    // 如果找到了 lrcButton
-    if (lrcButton) {
-      // 延迟1毫秒执行点击操作
-      setTimeout(function () {
-        lrcButton.click();
-      }, 1);
-      // 断开 MutationObserver 实例，停止监听 DOM 的变化
-      observer.disconnect();
-    }
-  });
-
   const adjustMenu = init => {
     const getAllWidth = ele => Array.from(ele).reduce((width, i) => width + i.offsetWidth, 0)
 
